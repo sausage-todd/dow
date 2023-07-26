f32c8fdb3d3c20f26f8f1cbc4eaf26943212d8d3
Mish Savelyev <michael.savelyev@gmail.com>
2023-07-26 13:05:39 +0200

    Install telnet on the machine

--- src/user_data.sh
+++ src/user_data.sh
@@ -18,6 +18,7 @@ apt install --yes \
   lsb-release \
   rsync \
   gcc \
+  inetutils-telnet \
   build-essential libssl-dev zlib1g-dev \
   libbz2-dev libreadline-dev libsqlite3-dev curl \
   libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
